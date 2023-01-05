f4e0efd9ac141a9a0163f808f547382af9b2e5c7
LvandoApps <lachlanvdk55@gmail.com>
2023-01-05 16:37:34 +1100

    begin Dxyn

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -248,7 +248,8 @@ void CHIP8::INSTRUCT_Cxkk() {
 // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
 // The interpreter reads n bytes from memory, starting at the address stored in I. These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). Sprites are XORed onto the existing screen. If this causes any pixels to be erased, VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is outside the coordinates of the display, it wraps around to the opposite side of the screen. See instruction 8xy3 for more information on XOR, and section 2.4, Display, for more information on the Chip-8 screen and sprites.
 void CHIP8::INSTRUCT_Dxyn() {
-    
+    uint8_t Vx = (instruction & x) >> 8u;
+    uint8_t Vy = (instruction & y) >> 4u;
 }
 
 // Skip next instruction if key with the value of Vx is pressed.
