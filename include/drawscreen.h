2d22460ee18f48821d20ec256c749e821a2919f2
LvandoApps <lachlanvdk55@gmail.com>
2023-01-14 14:19:10 +1100

    constructor, destructor, and update complete

--- include/drawscreen.h
+++ include/drawscreen.h
@@ -1,18 +1,16 @@
 #include <stdint.h>
 
-const unsigned int WINDOW_HEIGHT = 640;
-const unsigned int WINDOW_WIDTH = 1280;
-
 class DRAWSCREEN {
     public:
         SDL_Window* window = nullptr;
         SDL_Renderer* renderer = nullptr;
+        SDL_Texture* texture = nullptr;
         bool sdlWork = true;
 
     public:
         DRAWSCREEN(char const* title, int screenHeight, int screenWidth, int textureHeight, int textureWidth);
         ~DRAWSCREEN();
-        void Update();
+        void Update(void const* buffer, int pitch);
         bool Process();
 
 };
--- src/drawscreen.cpp
+++ src/drawscreen.cpp
@@ -2,27 +2,43 @@
 #include <SDL2/SDL.h>
 #include "../include/drawscreen.h"
 
-DRAWSCREEN::DRAWSCREEN(char const* title, int screenHeight, int screenWidth, int textureHeight, int textureWidth) {
+DRAWSCREEN::DRAWSCREEN(char const* title, int screen_height, int screen_width, int texture_height, int texture_width) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         sdlWork = false;
     }
     window = SDL_CreateWindow(
-        "LvandoApps CHIP-8 Emulator",
+        title,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        WINDOW_HEIGHT,
-        WINDOW_WIDTH,
+        screen_height,
+        screen_width,
         SDL_WINDOW_ALLOW_HIGHDPI
+    );
+    renderer = SDL_CreateRenderer(
+        window, 
+        -1, 
+        SDL_RENDERER_ACCELERATED
+    );
+    texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        texture_height,
+        texture_width
     );
 
 }
 
 DRAWSCREEN::~DRAWSCREEN() {
-    SDL_DestroyWindow(window);
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
 }
 
-int main() {
-    
+void DRAWSCREEN::Update(void const* buffer, int pitch) {
+    SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+    SDL_RenderPresent(renderer);
 }
